f1cb991ec829a316a9f014cbaf7ac5e363995c84
Walidoux <ma.walidkorchi@icloud.com>
2026-02-23 16:00:20 +0000

    feat(objects/TNT): implement tnt fly

---
+++ src/main/java/io/gihtub/rush/objects/BlockLoc.java
@@ -0,0 +1,76 @@
+package io.gihtub.rush.objects;
+
+import java.util.Objects;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+public class BlockLoc {
+
+    private final String world;
+    private final int x;
+    private final int y;
+    private final int z;
+
+    public BlockLoc(String world, int x, int y, int z) {
+        this.world = Objects.requireNonNull(world, "world");
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public BlockLoc(Location location) {
+        this(location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ());
+    }
+
+    public BlockLoc(Block block) {
+        this(block.getWorld().getName(), block.getX(), block.getY(), block.getZ());
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public boolean isInChunk(Chunk chunk) {
+        return x >> 4 == chunk.getX() && z >> 4 == chunk.getZ() && world.equals(chunk.getWorld().getName());
+    }
+
+    public boolean blockEquals(Block block) {
+        return x == block.getX() && y == block.getY() && z == block.getZ() && world.equals(block.getWorld().getName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BlockLoc that = (BlockLoc) o;
+        return x == that.x && y == that.y && z == that.z && world.equals(that.world);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(world, x, y, z);
+    }
+
+    @Override
+    public String toString() {
+        return "BlockLocation{world='" + world + "', x=" + x + ", y=" + y + ", z=" + z + '}';
+    }
+}
---
+++ src/main/java/io/gihtub/rush/objects/TNT.java
@@ -0,0 +1,215 @@
+package io.gihtub.rush.objects;
+
+import io.gihtub.rush.Main;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.Event;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.Event.Result;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import net.kyori.adventure.text.Component;
+
+class TNT implements Listener {
+
+    private static final Set<Material> EXPLOSION_IMMUNE_BLOCKS = Set.of(
+            Material.SANDSTONE,
+            Material.END_STONE);
+
+    private final Main plugin;
+
+    public TNT(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        final Player player = event.getPlayer();
+        final ItemStack item = event.getItem();
+        final Block block = event.getClickedBlock();
+
+        if (item != null) {
+            if (event.getAction() == Action.RIGHT_CLICK_BLOCK && block != null
+                    && block.getType() == Material.TNT
+                    && event.useInteractedBlock() != Result.DENY) {
+                block.setType(Material.AIR);
+                spawnTNT(block.getLocation(), player);
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER) {
+            if (event.getCause() == DamageCause.FALL) {
+                event.setDamage(event.getDamage() / plugin.getConfig().getDouble("fallDamage"));
+            } else if (event.getCause() == DamageCause.BLOCK_EXPLOSION) {
+                event.setDamage(event.getDamage() / plugin.getConfig().getDouble("TNTDamage"));
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getEntityType() == EntityType.PLAYER && isTNT(event.getDamager())) {
+            if (isPlayerIRG((Player) event.getEntity())) {
+                event.setDamage(event.getDamage() / plugin.getConfig().getDouble("TNTDamage"));
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityExplode(EntityExplodeEvent event) {
+        final Entity entity = event.getEntity();
+
+        if (isTNT(entity)) {
+            final Entity source = entity instanceof TNTPrimed ? ((TNTPrimed) entity).getSource() : null;
+
+            handleExplosion(event, source, event.getLocation(), event.getYield(), event.blockList());
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockExplode(BlockExplodeEvent event) {
+        handleExplosion(event, null, event.getBlock().getLocation(), event.getYield(), event.blockList());
+    }
+
+    public boolean isTNT(Entity entity) {
+        return entity instanceof TNTPrimed;
+    }
+
+    private void handleExplosion(Event event, Entity source, Location location,
+            float yield, List<Block> blockList) {
+        // if (plugin.getConfig().getBoolean("DisableDrops")) {
+        // yield = 0;
+
+        // if (event instanceof EntityExplodeEvent) {
+        // ((EntityExplodeEvent) event).setYield(0);
+        // } else {
+        // ((BlockExplodeEvent) event).setYield(0);
+        // }
+        // }
+
+        final Iterator<Block> blockIterator = blockList.iterator();
+        while (blockIterator.hasNext()) {
+            final Block block = blockIterator.next();
+            final Material blockType = block.getType();
+
+            if (blockType == Material.TNT) {
+                block.setType(Material.AIR);
+                spawnTNT(block.getLocation(), source);
+                blockIterator.remove();
+            } else if (plugin.getConfig().getBoolean("DisableBreak") || EXPLOSION_IMMUNE_BLOCKS.contains(blockType)) {
+                blockIterator.remove();
+            }
+
+            if (block.getType() == Material.OBSIDIAN) {
+                if (yield > 0) {
+                    block.breakNaturally();
+                } else {
+                    block.setType(Material.AIR);
+                }
+            }
+        }
+    }
+
+    public TNTPrimed spawnTNT(Location location, Entity source) {
+        final TNTPrimed tnt = location.getWorld().spawn(location.add(0.5, 0.25, 0.5), TNTPrimed.class);
+
+        tnt.setVelocity(new Vector(0, 0.25, 0));
+        tnt.teleport(location);
+        tnt.setIsIncendiary(false);
+        tnt.setFuseTicks(plugin.getConfig().getInt("explodeTicks"));
+        tnt.setYield((float) plugin.getConfig().getDouble("radius"));
+        tnt.setCustomNameVisible(true);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                final String timeLeft = new DecimalFormat("0.0").format(tnt.getFuseTicks() / 20.0);
+
+                tnt.customName(Component.text(timeLeft.concat("s"))); // i.e: 5s
+
+                if (!tnt.isValid() || tnt.getFuseTicks() <= 0) {
+                    cancel();
+                }
+            }
+        }.runTaskTimer(plugin, 0, 1);
+
+        if (source != null) {
+            try {
+                setTntSource(tnt, source);
+            } catch (ReflectiveOperationException event) {
+                plugin.getLogger().warning(
+                        "Cannot set the source for " + tnt + " (" + event.getClass().getName() + "): "
+                                + event.getMessage());
+            }
+        }
+        return tnt;
+    }
+
+    public static void setTntSource(TNTPrimed tnt, Entity source) throws ReflectiveOperationException {
+        if (hasSetSourceMethod()) {
+            tnt.setSource(source);
+            return;
+        }
+
+        // Old Bukkit versions support
+        final Method tntGetHandle = tnt.getClass().getDeclaredMethod("getHandle");
+        final Method entityGetHandle = source.getClass().getDeclaredMethod("getHandle");
+
+        final Object craftTnt = tntGetHandle.invoke(tnt);
+        final Object craftEntity = entityGetHandle.invoke(source);
+
+        final Field sourceField = craftTnt.getClass().getDeclaredField("source");
+
+        sourceField.setAccessible(true);
+        sourceField.set(craftTnt, craftEntity);
+    }
+
+    private static boolean hasSetSourceMethod() {
+        try {
+            TNTPrimed.class.getMethod("setSource", Entity.class);
+            return true;
+        } catch (NoSuchMethodException event) {
+            return false;
+        }
+    }
+
+    /**
+     * Checks if the rush game has started and the player is in the rush world.
+     *
+     * @param player the player to check
+     */
+    public boolean isPlayerIRG(Player player) {
+        if (!plugin.isGameStarted()) {
+            return false;
+        } else {
+            return player.getWorld().getName().equals(plugin.getGameWorld());
+        }
+    }
+}
