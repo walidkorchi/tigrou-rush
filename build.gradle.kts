122cb575a43eb007a20c70e64220f855028bf562
Walidoux <ma.walidkorchi@icloud.com>
2026-02-23 15:22:09 +0000

    refactor: update docs + clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,11 +9,6 @@ repositories {
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
-        content {
-            includeModule("io.papermc.paper", "paper-api")
-            includeModule("io.papermc", "paperlib")
-            includeGroup("io.papermc.adventure")
-        }
     }
 
     maven {
@@ -24,14 +19,34 @@ repositories {
         }
     }
 
+    maven {
+        name = "enginehub"
+        url = uri("https://maven.enginehub.org/repo/")
+    }
+
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly(files("../rush-tl-events/plugins/worldedit-bukkit-7.4.0.jar"))
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.shadowJar {
+    archiveFileName.set("TigrouRush.jar")
+}
+
+tasks.register<Copy>("copyToPlugins") {
+    dependsOn("shadowJar")
+    from(tasks.shadowJar)
+    into(file("../rush-tl-events/plugins"))
+}
+
+tasks.build {
+    dependsOn("copyToPlugins")
 }
